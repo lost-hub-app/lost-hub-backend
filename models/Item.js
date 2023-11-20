58072ab2cbd7076770e1dc31adabb3547f69b75a
Chester Lee Coloma <cleecoloma@gmail.com>
2023-11-19 21:07:25 -0800

    updated Item Schema

--- models/Item.js
+++ models/Item.js
@@ -4,13 +4,14 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const itemSchema = new Schema({
-  description: { type: String, required: true },
+  type: { type: String, enum: ['lost', 'found'], required: true },
+  itemName: { type: String, required: true },
+  image: { type: String, required: true },
   location: { type: String, required: true },
-  date: { type: Date, default: Date.now },
-  categoryID: { type: Schema.Types.ObjectId, ref: 'Category' },
+  description: { type: String, required: true },
+  // date: { type: Date, default: Date.now },
+  // categoryID: { type: Schema.Types.ObjectId, ref: 'Category' },
   userID: { type: Schema.Types.ObjectId, ref: 'User' },
-  status: { type: String, enum: ['lost', 'found'], required: true },
-  photoURL: String
 });
 
 const ItemModel = mongoose.model('Item', itemSchema);
